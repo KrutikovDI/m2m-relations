5b58794efacb9dadab24daa928645da49245825b
Danil Krutikov <kdanil.i@yandex.ru>
2023-06-08 16:49:58 +0700

    Initial commit

---
+++ articles/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+class Tag(models.Model):
+    name = models.CharField(max_length=256)
+
+class Article(models.Model):
+
+    title = models.CharField(max_length=256, verbose_name='Название')
+    text = models.TextField(verbose_name='Текст')
+    published_at = models.DateTimeField(verbose_name='Дата публикации')
+    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    tags = models.ManyToManyField(Tag, related_name='articles', verbose_name='Разделы')
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
+
+    def __str__(self):
+        return self.title
+
+class Scope(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='scopes', verbose_name='Раздел')
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
+    is_main = models.BooleanField(verbose_name='Основной')
