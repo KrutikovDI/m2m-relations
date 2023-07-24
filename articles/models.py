b99d33a816ac99dcf0a902c68e3e559e1331cd72
Danil Krutikov <kdanil.i@yandex.ru>
2023-07-24 23:54:04 +0700

    Second commit

--- articles/models.py
+++ articles/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class Tag(models.Model):
     name = models.CharField(max_length=256)
+    def __str__(self):
+        return self.name
 
 class Article(models.Model):
 
